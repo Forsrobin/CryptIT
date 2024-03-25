d79e8519bc8dfe86e0e988d2e9223698c4d1f986
Forsrobin <forsrobin@gmail.com>
2024-03-25 22:41:50 +0100

    Add build folder creation and update CMake presets

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-conan install . --build=missing --profile=default 
+# Id the buuild folder is not present, create it
+if [ ! -d "build" ]; then
+  mkdir build
+fi
 
+conan install . --build=missing --profile=default 
+cmake --preset vs2022-rel
+cmake --build --preset vs2022-rel
 rm CMakeUserPresets.json
--- debug_build.sh
+++ debug_build.sh
@@ -1,3 +1,11 @@
+# Id the buuild folder is not present, create it
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
 conan install . --build=missing --profile=debug 
+cmake --preset vs2022-deb
+cmake --build --preset vs2022-deb
+ctest --preset vs2022-deb
 
 rm CMakeUserPresets.json
