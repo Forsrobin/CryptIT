8eca3276f49cf82d514caee49719e25b2f7989bb
Forsrobin <forsrobin@gmail.com>
2024-03-27 16:26:48 +0100

    Update build and installation process

--- debug_build.sh
+++ debug_build.sh
@@ -3,8 +3,6 @@ if [ ! -d "build" ]; then
   mkdir build
 fi
 
-conan install . --build=missing -s build_type=Debug 
-cmake --preset vs2022-deb
-cmake --build --preset vs2022-deb
+conan install . --build=missing --profile=Debug
 
 rm CMakeUserPresets.json
