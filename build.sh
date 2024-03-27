e2930b10fb2ad7ca4706db08b2f54f18f45758fe
Robin Fors <39378839+Forsrobin@users.noreply.github.com>
2024-03-27 22:19:30 +0100

    Continues Integraion (#1)
    * Update branch names in GitHub workflow
    
    * Update CMake configuration and remove unnecessary script
    
    * Update CMake configuration and add platform-specific build options
    
    * Add clang and clang++ installation for Ubuntu
    
    * Update build matrix and add Ubuntu-specific steps
    
    * Clang path added
    
    * Remove gtest/1.14.0 requirement from conanfile.txt
    
    * Test
    
    * Commented out unused steps in cmake-multi-platform.yml
    
    * Update CMake configuration and Conan profile***
    
    * Update compiler settings in setup.sh
    
    * Update CMake workflow and dependencies
    
    * Remove unnecessary platform specifier in CMakeLists.txt
    
    * Add Qt6::QWindowsIntegrationPlugin to target_link_libraries for Windows
    
    * Disable console window on Windows for GUI application
    
    * Refactor CMake configuration and add ls commands for debugging
    
    * Refactor test directory path in CMake workflow
    
    * Removed LS
    
    * Remove unnecessary ls command in workflow file
    
    * Add BUILD_TESTS flag to CMake configuration
    
    * Update workflow names
    
    * Add path message for tests subdirectory
    
    * Update build and installation process
    
    * Update build-debug-test.yml and conanfile.txt
    
    * Enable Free Disk Space check on Ubuntu
    
    * Update CMake flags in build configuration
    
    * Remove unnecessary test files
    
    * Add CMakeLists.txt for tests
    
    * Update build output directory path
    
    * Fix build-output-dir path in build-debug-test.yml
    
    * Refactor build configuration and test directory***
    
    * Update build-debug-test.yml to activate conan environment based on OS
    
    * Remove unnecessary comment in CMake configuration
    
    * Refactor build scripts and presets
    
    * Update build and test workflow
    
    * Add build_type parameter to Windows job in build-and-test.yml
    
    * Refactor reusable strings setup in build-and-test.yml
    
    * Update build-and-test.yml with cmake configuration for Linux and Windows
    
    * Fix build and test workflow***
    
    ***Added 'Tree' step for Windows***
    ***Updated comments for clarity***
    ***Fixed indentation in CMake configuration and build steps***
    ***Fixed indentation in test execution step
    
    * Fix CMAKE_TOOLCHAIN_FILE path in build-and-test.yml
    
    * Update CMAKE_TOOLCHAIN_FILE path in build-and-test.yml
    
    * Update build-and-test.yml: Changed 'Tree' step to 'Dir' step for Windows
    
    * Update build-and-test.yml to use PowerShell command for listing files on Windows
    
    * Fix CMAKE_TOOLCHAIN_FILE path in build script
    
    * Remove unnecessary 'Dir' step in build-and-test workflow
    
    * Update CMAKE_TOOLCHAIN_FILE path in build-and-test.yml
    
    * Fix CMAKE_TOOLCHAIN_FILE path in build-and-test.yml
    
    * Update branch name in workflow file

--- build.sh
+++ build.sh
@@ -3,8 +3,18 @@ if [ ! -d "build" ]; then
   mkdir build
 fi
 
-conan install . --build=missing --profile=default 
-cmake --preset vs2022-rel
-cmake --build --preset vs2022-rel
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied, pass either rel or deb"
+    exit 1
+fi
 
-rm CMakeUserPresets.json
+if [ $1 == "rel" ]; then
+    conan install . --build=missing --profile=default 
+    rm CMakeUserPresets.json
+fi
+
+if [ $1 == "deb" ]; then
+    conan install . --build=missing --profile=Debug
+    rm CMakeUserPresets.json
+fi
+
--- setup.sh
+++ setup.sh
@@ -8,10 +8,9 @@ fi
 if [ $1 == "ubuntu-latest" ]; then
     # Install all the dependencies you nned to build the project
 
-
-echo "[settings]
+    echo "[settings]
 arch=x86_64
-build_type=Debug
+build_type=$2
 compiler=gcc
 compiler.cppstd=gnu17
 compiler.libcxx=libstdc++11
@@ -25,7 +24,32 @@ tools.system.package_manager:sudo=True" > ~/.conan2/profiles/$1
     ls -la ~/
     ls -la ~/.conan2/
     ls -la ~/.conan2/profiles/
-    
+
+    echo "Checking the content of the profile file: ~/.conan2/profiles/$1"
+    cat ~/.conan2/profiles/$1
+
+fi
+
+# Check if the first argument is "ubuntu-latest"
+if [ $1 == "windows-latest" ]; then
+    # Install all the dependencies you nned to build the project
+
+    echo "[settings]
+arch=x86_64
+build_type=$2
+compiler=msvc
+compiler.cppstd=23
+compiler.runtime=dynamic
+compiler.version=193
+os=Windows
+
+[conf]
+tools.system.package_manager:mode=install" > ~/.conan2/profiles/$1
+
+    ls -la ~/
+    ls -la ~/.conan2/
+    ls -la ~/.conan2/profiles/
+
     echo "Checking the content of the profile file: ~/.conan2/profiles/$1"
     cat ~/.conan2/profiles/$1
 
